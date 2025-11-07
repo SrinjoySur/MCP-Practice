dd76b7fefbaa98c34dd68f7ec834dd33e32949ea
Srinjoy Sur <srinjoy_sur@epam.com>
2025-11-07 10:58:35 +0530

    chore: improved the greeting server

--- greeting_server.py
+++ greeting_server.py
@@ -1,9 +1,13 @@
 from fastmcp import FastMCP
 
 # Create the MCP server
-mcp = FastMCP("Greeting Server")
+mcp = FastMCP(
+    name="Greeting Server",
+    instructions="""
+        Greets Any User Who Comes To The Server.
+""")
 
-@mcp.tool
+@mcp.tool(name="Greet",description="Greets the user")
 def greet(name: str) -> str:
     return f"Hello, {name}!"
 
